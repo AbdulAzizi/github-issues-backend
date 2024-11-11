89fc25f333593504ef473415702826fc2db05c7b
AbdulAziz Nurov <nurovaziz@gmail.com>
2024-11-11 23:00:42 +0500

    nest g module github

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GithubModule } from './github/github.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
+    GithubModule,
   ],
   controllers: [AppController],
   providers: [AppService],
