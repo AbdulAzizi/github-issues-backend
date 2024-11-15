55cfe33c9ac205bff13fbabf0707622958dc958e
AbdulAziz Nurov <nurovaziz@gmail.com>
2024-11-15 09:35:36 +0500

    In log store request propper type

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -8,7 +8,17 @@ export class LoggerMiddleware implements NestMiddleware {
 
   async use(req: Request, res: Response, next: NextFunction) {
     const ip = req.ip;
-    const requestType = req.method;
+
+    const routeMap: Record<string, string> = {
+      '/github/issues': 'get_issues',
+      '/github/issue': 'get_issue',
+    };
+
+    const requestPath =
+      Object.keys(routeMap).find((path) => req.originalUrl.startsWith(path)) ||
+      req.originalUrl;
+    const requestType = routeMap[requestPath] || requestPath;
+
     const details = { url: req.url, headers: req.headers };
 
     await this.logsService.logRequest(ip, requestType, details);
